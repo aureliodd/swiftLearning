b912e4235d65ed0efb7f8f7a784e431046b2e888
Aurelio <aureliomsn@hotmail.it>
2023-05-01 23:55:44 +0200

    Initial Commit

---
+++ swiftLearning/swiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftLearningApp.swift
+//  swiftLearning
+//
+//  Created by Aurelio D'urso on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
